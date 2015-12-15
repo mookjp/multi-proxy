7d8e4100c34763bf0737f5e3190695ccd7f81bb3
mook <mookjpy@gmail.com>
2015-12-16 02:04:59 +0900

    Fix example to show replica origin servers pattern

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ multiproxy
 💻 👉👉        👉👉💻
         👉💻 👉
 ```
+
+## Run examples
+
+```
+npm install --global babel-cli
+
+# Run proxy server with origin servers
+babel-node --presets es2015,stage-2 -- example/app.js
+
+# Send request to proxy server
+curl -v -XGET localhost:9999/index/hoge/fuga
+```
--- example/app.js
+++ example/app.js
@@ -1,19 +1,64 @@
 import http from 'http';
 import connect from 'connect';
 
-// Create destination list
-const servers = [
-  'http://localhost:9200',
-  'http://localhost:9210'
-];
+import ProxyServer from '../src/main/middleware/proxy';
+
+// Create fake destination servers
+let destinationOrig = connect();
+destinationOrig.use((req, res, next) => {
+  "use strict";
+  res.writeHead(200, 'Content-Type: text/plain');
+  res.end('\nDestination original!');
+  next();
+});
+let destinationFirst = connect();
+destinationFirst.use((req, res, next) => {
+  "use strict";
+  res.writeHead(200, 'Content-Type: text/plain');
+  res.end('\nDestination first!');
+  next();
+});
+let destinationSecond = connect();
+destinationSecond.use((req, res, next) => {
+  res.writeHead(200, 'Content-Type: text/plain');
+  res.end('\nDestination second!');
+  next();
+});
+
+// Stert to listen
+const origPort = 4410;
+const firstPort = 4411;
+const secondPort = 4412;
+http.createServer(destinationOrig).listen(origPort);
+http.createServer(destinationFirst).listen(firstPort);
+http.createServer(destinationSecond).listen(secondPort);
 
 // Create url patterns to send requests to some destinations
-const patterns = [];
+const patterns = [
+  /^\/index/,
+  /-suffix$/
+];
 
-let app = connect();
+// Create destination list
+const servers = {
+  // master should be a single endpoint
+  //master: `http://localhost:${origPort}`,
+
+  // replica can have several endpoints
+  replica: [
+    `http://localhost:${firstPort}`,
+    `http://localhost:${secondPort}`
+  ]
+};
+
 let proxy = new ProxyServer(servers, patterns);
-app.use(proxy.proxyRequest);
+let app = connect();
+app.use((req, res, next) => {
+  "use strict";
+  // Use inside the function because of "this" problem
+  proxy.proxyRequest(req, res, next);
+});
 
-const port = 9999;
-http.createServer(app).listen(port);
-console.log(`Proxy server is listening on port ${port}`);
+const proxyPort = 9999;
+http.createServer(app).listen(proxyPort);
+console.log(`Proxy server is listening on port ${proxyPort}`);
